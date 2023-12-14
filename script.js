84e384d2e4cd1e154052ee4467cfcc5d9ec4abd3
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-12-14 20:26:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-//complete this code
-class Animal {}
+class Animal {
+    constructor(species) {
+        this._species = species;
+    }
 
-class Dog extends Animal {}
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    makeSound() {
+        console.log("The animal makes a sound");
+    }
+}
+
+class Cat extends Animal {
+    constructor(species) {
+        super(species);
+    }
+
+    purr() {
+        console.log("purr");
+    }
+}
+
+class Dog extends Animal {
+    constructor(species) {
+        super(species);
+    }
+
+    bark() {
+        console.log("woof");
+    }
+}
+
+
+
+
+// //complete this code
+// class Animal {}
+
+// class Dog extends Animal {}
+
+// class Cat extends Animal {}
 
 // Do not change the code below this line
 window.Animal = Animal;
